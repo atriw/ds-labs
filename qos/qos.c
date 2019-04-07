41f468976798d5832efa49506e4c66574a7dfb28
Li Jun <875241499@qq.com>
2019-04-07 22:40:44 +0800

    config 14

--- qos/qos.c
+++ qos/qos.c
@@ -15,10 +15,10 @@ qos_meter_init(void)
 {
     /* to do */
     struct rte_meter_srtcm_params app_srtcm_params[APP_FLOWS_MAX] = {
-        {.cir = 160000000,  .cbs = 160000000, .ebs = 160000000},
-        {.cir = 80000000,  .cbs = 80000000, .ebs = 80000000},
-        {.cir = 40000000,  .cbs = 40000000, .ebs = 40000000},
-        {.cir = 20000000,  .cbs = 20000000, .ebs = 20000000}
+        {.cir = 160000000000,  .cbs = 120000, .ebs = 40000},
+        {.cir = 80000000000,  .cbs = 60000, .ebs = 20000},
+        {.cir = 40000000000,  .cbs = 30000, .ebs = 10000},
+        {.cir = 20000000000,  .cbs = 15000, .ebs = 5000}
     };
     int ret;
 
@@ -53,23 +53,23 @@ qos_dropper_init(void)
     /* to do */
     struct rte_red_params app_red_params[APP_FLOWS_MAX][e_RTE_METER_COLORS] = {
         {
-            {.min_th = 1000, .max_th = 1001, .maxp_inv = 10, .wq_log2 = 8},
-            {.min_th = 1, .max_th = 500, .maxp_inv = 5, .wq_log2 = 8},
+            {.min_th = 750, .max_th = 751, .maxp_inv = 10, .wq_log2 = 8},
+            {.min_th = 1, .max_th = 250, .maxp_inv = 5, .wq_log2 = 8},
             {.min_th = 1, .max_th = 2, .maxp_inv = 1, .wq_log2 = 8}
         },
         {
-            {.min_th = 500, .max_th = 501, .maxp_inv = 10, .wq_log2 = 4},
-            {.min_th = 1, .max_th = 250, .maxp_inv = 5, .wq_log2 = 4},
+            {.min_th = 375, .max_th = 376, .maxp_inv = 10, .wq_log2 = 4},
+            {.min_th = 1, .max_th = 125, .maxp_inv = 5, .wq_log2 = 4},
             {.min_th = 1, .max_th = 2, .maxp_inv = 1, .wq_log2 = 4}
         },
         {
-            {.min_th = 250, .max_th = 251, .maxp_inv = 10, .wq_log2 = 2},
-            {.min_th = 1, .max_th = 125, .maxp_inv = 5, .wq_log2 = 2},
+            {.min_th = 187, .max_th = 188, .maxp_inv = 10, .wq_log2 = 2},
+            {.min_th = 1, .max_th = 62, .maxp_inv = 5, .wq_log2 = 2},
             {.min_th = 1, .max_th = 2, .maxp_inv = 1, .wq_log2 = 2}
         },
         {
-            {.min_th = 125, .max_th = 126, .maxp_inv = 10, .wq_log2 = 1},
-            {.min_th = 1, .max_th = 62, .maxp_inv = 5, .wq_log2 = 1},
+            {.min_th = 93, .max_th = 94, .maxp_inv = 10, .wq_log2 = 1},
+            {.min_th = 1, .max_th = 31, .maxp_inv = 5, .wq_log2 = 1},
             {.min_th = 1, .max_th = 2, .maxp_inv = 1, .wq_log2 = 1}
         }
     };
