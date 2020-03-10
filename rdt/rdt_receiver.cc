3aa76b80918a502e78150f515ec3bdb50993b018
artor1os <875241499@qq.com>
2020-03-10 17:56:00 +0800

    Initialize

--- rdt/rdt_receiver.cc
+++ rdt/rdt_receiver.cc
@@ -6,8 +6,8 @@
  *       situations.  In this implementation, the packet format is laid out as 
  *       the following:
  *       
- *       |<-  1 byte  ->|<-      4 byte     ->|<-  2 byte  ->|<-        the rest        ->|
- *       | payload size |<- sequence number ->|<- checksum ->|<-        payload         ->|
+ *       |<-  1 byte  ->|<-             the rest            ->|
+ *       | payload size |<-             payload             ->|
  *
  *       The first byte of each packet indicates the size of the payload
  *       (excluding this single-byte header)
@@ -20,156 +20,6 @@
 
 #include "rdt_struct.h"
 #include "rdt_receiver.h"
-
-typedef unsigned int seq_nr;
-#define MAX_SEQ 4294967295
-#define PAYLOAD_HEADER_SIZE sizeof(char)
-#define SEQ_SIZE sizeof(unsigned int)
-#define CHECKSUM_SIZE sizeof(unsigned short)
-#define get_seq(pkt) (*((seq_nr *)((pkt)->data + PAYLOAD_HEADER_SIZE)))
-#define HEADER_SIZE (PAYLOAD_HEADER_SIZE + SEQ_SIZE + CHECKSUM_SIZE)
-
-struct pkt_node
-{
-    packet pkt;
-    pkt_node *next = NULL;
-};
-
-static seq_nr seq = 0;
-
-static void inc_seq()
-{
-    if (seq < MAX_SEQ) {
-        seq++;
-    } else {
-        seq = 0;
-    }
-}
-
-static int count(pkt_node *list)
-{
-    if (list == NULL) return 0;
-    return 1 + count(list->next);
-}
-
-static unsigned short gen_checksum(char *buf, int nword)
-{
-    unsigned long sum;
- 
-    for(sum = 0; nword > 0; nword--)
-        sum += *buf++;             
- 
-    sum  = (sum>>16) + (sum&0xffff);
-    sum += (sum>>16);
-
-    return ~sum;
-}
-
-static bool verify_checksum(char *buf, int nword, unsigned short checksum)
-{
-    unsigned long sum;
- 
-    for(sum = 0; nword > 0; nword--)
-        sum += *buf++;             
- 
-    sum  = (sum>>16) + (sum&0xffff);
-    sum += (sum>>16);
-
-    return (((sum + checksum) & 0xff) == 0xff);
-}
-
-static pkt_node *pkt_list = NULL;
-static pkt_node *pkt_tail = NULL;
-
-static pkt_node *pkt_new(packet *pkt)
-{
-    pkt_node *node = (pkt_node *)malloc(sizeof(*node));
-    memcpy(&node->pkt, pkt, sizeof(packet));
-    node->next = NULL;
-    return node;
-}
-
-static void suspend(packet *pkt)
-{
-    pkt_node *node = pkt_new(pkt);
-    if (pkt_list == NULL) {
-        pkt_list = pkt_tail = node;
-    } else {
-        for (pkt_node *p=pkt_list, *prev = NULL; p; prev = p, p=p->next) {
-            seq_nr p_seq = *((seq_nr *)(p->pkt.data + 1));
-            seq_nr pkt_seq = *((seq_nr *)(pkt->data + 1));
-            if (pkt_seq == p_seq) {
-                free(node);
-                return;
-            } else if (pkt_seq < p_seq) {
-                node->next = p;
-                if (prev) {
-                    prev->next = node;
-                } else {
-                    pkt_list = node;
-                }
-                return;
-            }
-        }
-        pkt_tail->next = node;
-        pkt_tail = pkt_tail->next;
-    }
-}
-
-static void consume(seq_nr k);
-
-static void send_ack(seq_nr k)
-{
-    packet ack;
-
-    int maxpayload_size = RDT_PKTSIZE - HEADER_SIZE;
-
-    ack.data[0] = maxpayload_size;
-    *((seq_nr *)(ack.data + 1)) = k;
-    memset(ack.data + HEADER_SIZE, 1, maxpayload_size);
-    unsigned short cs = gen_checksum(ack.data+HEADER_SIZE, maxpayload_size);
-    *((unsigned short *)(ack.data + PAYLOAD_HEADER_SIZE + SEQ_SIZE)) = cs;
-    Receiver_ToLowerLayer(&ack);
-}
-
-static void confirm(packet *pkt)
-{
-    /* 1-byte header indicating the size of the payload */
-    int header_size = HEADER_SIZE;
-
-    /* construct a message and deliver to the upper layer */
-    struct message *msg = (struct message*) malloc(sizeof(struct message));
-    ASSERT(msg!=NULL);
-
-    msg->size = pkt->data[0];
-
-    /* sanity check in case the packet is corrupted */
-    if (msg->size<0) msg->size=0;
-    if (msg->size>RDT_PKTSIZE-header_size) msg->size=RDT_PKTSIZE-HEADER_SIZE;
-
-    msg->data = (char*) malloc(msg->size);
-    ASSERT(msg->data!=NULL);
-    memcpy(msg->data, pkt->data+HEADER_SIZE, msg->size);
-    Receiver_ToUpperLayer(msg);
-
-    /* don't forget to free the space */
-    if (msg->data!=NULL) free(msg->data);
-    if (msg!=NULL) free(msg);
-
-    send_ack(seq);
-    inc_seq();
-    consume(seq);
-}
-
-static void consume(seq_nr k)
-{
-    if (pkt_list && (*((seq_nr *)(pkt_list->pkt.data + 1))) == k) {
-        pkt_node *p = pkt_list;
-        pkt_list = pkt_list->next;
-        confirm(&p->pkt);
-        free(p);
-    }
-}
 
 
 /* receiver initialization, called once at the very beginning */
@@ -191,18 +41,25 @@ void Receiver_Final()
    receiver */
 void Receiver_FromLowerLayer(struct packet *pkt)
 {
-    int payload_size = pkt->data[0];
-    seq_nr actual_seq = *((seq_nr *)(pkt->data + PAYLOAD_HEADER_SIZE));
+    /* 1-byte header indicating the size of the payload */
+    int header_size = 1;
 
-    unsigned short checksum = *((unsigned short *)(pkt->data + PAYLOAD_HEADER_SIZE + SEQ_SIZE));
-    if (!verify_checksum(pkt->data+HEADER_SIZE, payload_size, checksum))
-        return;
+    /* construct a message and deliver to the upper layer */
+    struct message *msg = (struct message*) malloc(sizeof(struct message));
+    ASSERT(msg!=NULL);
 
-    if (actual_seq > seq) {
-        suspend(pkt);
-    } else if (actual_seq == seq) {
-        confirm(pkt);
-    } else {
-        send_ack(actual_seq);
-    }
+    msg->size = pkt->data[0];
+
+    /* sanity check in case the packet is corrupted */
+    if (msg->size<0) msg->size=0;
+    if (msg->size>RDT_PKTSIZE-header_size) msg->size=RDT_PKTSIZE-header_size;
+
+    msg->data = (char*) malloc(msg->size);
+    ASSERT(msg->data!=NULL);
+    memcpy(msg->data, pkt->data+header_size, msg->size);
+    Receiver_ToUpperLayer(msg);
+
+    /* don't forget to free the space */
+    if (msg->data!=NULL) free(msg->data);
+    if (msg!=NULL) free(msg);
 }
--- rdt/rdt_sender.cc
+++ rdt/rdt_sender.cc
@@ -6,8 +6,8 @@
  *       situations.  In this implementation, the packet format is laid out as 
  *       the following:
  *       
- *       |<-  1 byte  ->|<-      4 byte     ->|<-  2 byte  ->|<-        the rest        ->|
- *       | payload size |<- sequence number ->|<- checksum ->|<-        payload         ->|
+ *       |<-  1 byte  ->|<-             the rest            ->|
+ *       | payload size |<-             payload             ->|
  *
  *       The first byte of each packet indicates the size of the payload
  *       (excluding this single-byte header)
@@ -17,201 +17,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <queue>
 
 #include "rdt_struct.h"
 #include "rdt_sender.h"
 
-#define WINDOW_SIZE 10
-#define MAX_SEQ 4294967295
-#define DEFAULT_TIMEOUT 0.3
-#define between(a, b, c) (((a <= b) && (b < c)) || ((c < a) && (a <= b)) || ((b < c) && (c < a)))
-#define PAYLOAD_HEADER_SIZE sizeof(char)
-#define SEQ_SIZE sizeof(unsigned int)
-#define CHECKSUM_SIZE sizeof(unsigned short)
-#define get_seq(pkt) (*((seq_nr *)((pkt)->data + PAYLOAD_HEADER_SIZE)))
-#define HEADER_SIZE (PAYLOAD_HEADER_SIZE + SEQ_SIZE + CHECKSUM_SIZE)
 
-typedef unsigned int seq_nr;
-
-//----------------------------------------------------------------
-// DEBUG
-seq_nr getseq(packet *pkt)
-{
-    return get_seq(pkt);
-}
-
-//----------------------------------------------------------------
-// vitural timer implemented on one physical timer
-struct vtimer
-{
-    double timeout;
-    double start;
-    seq_nr num;
-    vtimer *next = NULL;
-};
-
-static vtimer *timer_list = NULL;
-static vtimer *timer_tail = NULL;
-
-static void destroy(vtimer *p)
-{
-    if (p == NULL) return;
-    destroy(p->next);
-    free(p);
-}
-
-static void append(vtimer *timer) 
-{
-    if (timer_list == NULL) {
-        timer_list = timer_tail = timer;
-    } else {
-        timer_tail->next = timer;
-        timer_tail = timer_tail->next;
-    }
-}
-
-static void pop()
-{
-    if (timer_list) {
-        vtimer *p = timer_list;
-        if (timer_tail == timer_list) 
-            timer_list = timer_tail = NULL;
-        else timer_list = timer_list->next;
-        free(p);
-    }
-}
-
-static vtimer *new_timer(double timeout, seq_nr k)
-{
-    vtimer *p = (vtimer *)malloc(sizeof(*p));
-    p->timeout = timeout;
-    p->start = GetSimulationTime();
-    p->num = k;
-    p->next = NULL;
-    return p;
-}
-
-static void register_timer(double timeout, seq_nr k)
-{
-    vtimer *p = new_timer(timeout, k);
-    append(p);
-    if (!Sender_isTimerSet()) 
-        Sender_StartTimer(timeout);
-}
-
-static void start_timer()
-{
-    ASSERT(timer_list != NULL);
-    double timeout = timer_list->timeout - (GetSimulationTime() - timer_list->start);
-    if (timeout > 0) {
-        Sender_StartTimer(timeout);
-    } else {
-        Sender_Timeout();
-    }
-}
-
-static void stop_timer(seq_nr k)
-{
-    for (vtimer *p=timer_list, *prev=NULL; p; prev = p, p=p->next) {
-        if (p->num == k) {
-            if (p == timer_list) {
-                Sender_StopTimer();
-                pop();
-                if (timer_list) start_timer();
-            } else {
-                prev->next = p->next;
-                free(p);
-            }
-            return;
-        }
-    }
-}
-
-//----------------------------------------------------------------
-
-static packet window[WINDOW_SIZE + 1];
-static seq_nr nbuffered = 0;
-static seq_nr seq = 0;
-static seq_nr ack = 0;
-
-static void inc_seq()
-{
-    if (seq < MAX_SEQ) {
-        seq++;
-    } else {
-        seq = 0;
-    }
-}
-
-static void inc_ack()
-{
-    if (ack < MAX_SEQ) {
-        ack++;
-    } else {
-        ack = 0;
-    }
-}
-//----------------------------------------------------------------
-// sender buffer
-struct buffer
-{
-    packet pkt;
-    buffer *next = NULL;
-};
-
-static buffer *buffer_list = NULL;
-static buffer *buffer_tail = NULL;
-
-static buffer *buffer_new(packet *pkt)
-{
-    buffer *p = (buffer *)malloc(sizeof(*p));
-    memcpy(&p->pkt, pkt, sizeof(packet));
-    p->next = NULL;
-    return p;
-}
-
-static void buffer_push(packet *pkt)
-{
-    buffer *p = buffer_new(pkt);
-    if (buffer_list == NULL) {
-        buffer_list = buffer_tail = p;
-    } else {
-        buffer_tail->next = p;
-        buffer_tail = buffer_tail->next;
-    }
-}
-
-static buffer *buffer_pop()
-{
-    if (buffer_list) {
-        buffer *p = buffer_list;
-        if (buffer_tail == buffer_list) 
-            buffer_list = buffer_tail = NULL;
-        else 
-            buffer_list = buffer_list->next;
-        return p;
-    }
-    return NULL;
-}
-
-static unsigned int buffer_count(buffer *list)
-{
-    if (list == NULL) return 0;
-    return 1 + buffer_count(list->next);
-}
-
-static bool buffer_empty()
-{
-    return buffer_list == NULL;
-}
-//----------------------------------------------------------------
 /* sender initialization, called once at the very beginning */
 void Sender_Init()
 {
-    nbuffered = 0;
-    seq = 0;
-    ack = 0;
     fprintf(stdout, "At %.2fs: sender initializing ...\n", GetSimulationTime());
 }
 
@@ -223,59 +36,16 @@ void Sender_Final()
 {
     fprintf(stdout, "At %.2fs: sender finalizing ...\n", GetSimulationTime());
 }
-//----------------------------------------------------------------
-static void send(packet *pkt)
-{
-    seq_nr seq_expected = get_seq(pkt);
-    memcpy(window+(seq_expected % WINDOW_SIZE), pkt, sizeof(packet));
-    nbuffered = nbuffered + 1;
-    
-	/* send it out through the lower layer */
-	Sender_ToLowerLayer(window+(seq_expected % WINDOW_SIZE));
-    register_timer(DEFAULT_TIMEOUT, seq_expected);
-}
 
-static void suspend(packet *pkt)
-{
-    buffer_push(pkt);
-}
-//----------------------------------------------------------------
-static unsigned short gen_checksum(char *buf, int nword)
-{
-    unsigned long sum;
- 
-    for(sum = 0; nword > 0; nword--)
-        sum += *buf++;             
- 
-    sum  = (sum>>16) + (sum&0xffff);
-    sum += (sum>>16);
- 
-    return ~sum;
-}
-
-static bool verify_checksum(char *buf, int nword, unsigned short checksum)
-{
-    unsigned long sum;
- 
-    for(sum = 0; nword > 0; nword--)
-        sum += *buf++;             
- 
-    sum  = (sum>>16) + (sum&0xffff);
-    sum += (sum>>16);
-
-    return (((sum + checksum) & 0xff) == 0xff);
-}
-//----------------------------------------------------------------
 /* event handler, called when a message is passed from the upper layer at the 
    sender */
 void Sender_FromUpperLayer(struct message *msg)
 {
     /* 1-byte header indicating the size of the payload */
-    // int header_size = PAYLOAD_HEADER_SIZE + SEQ_SIZE + CHECKSUM_SIZE;
-    
+    int header_size = 1;
 
     /* maximum payload size */
-    int maxpayload_size = RDT_PKTSIZE - HEADER_SIZE;
+    int maxpayload_size = RDT_PKTSIZE - header_size;
 
     /* split the message if it is too big */
 
@@ -288,18 +58,10 @@ void Sender_FromUpperLayer(struct message *msg)
     while (msg->size-cursor > maxpayload_size) {
 	/* fill in the packet */
 	pkt.data[0] = maxpayload_size;
-    *((seq_nr *)(pkt.data + PAYLOAD_HEADER_SIZE)) = seq;
-	memcpy(pkt.data+HEADER_SIZE, msg->data+cursor, maxpayload_size);
-    // set checksum
-    unsigned short cs = gen_checksum(pkt.data+HEADER_SIZE, maxpayload_size);
-    *((unsigned short *)(pkt.data + PAYLOAD_HEADER_SIZE + SEQ_SIZE)) = cs;
+	memcpy(pkt.data+header_size, msg->data+cursor, maxpayload_size);
 
-    if (nbuffered < WINDOW_SIZE) {
-        send(&pkt);
-    } else { // window is full
-        suspend(&pkt);
-    }
-    inc_seq();
+	/* send it out through the lower layer */
+	Sender_ToLowerLayer(&pkt);
 
 	/* move the cursor */
 	cursor += maxpayload_size;
@@ -309,18 +71,10 @@ void Sender_FromUpperLayer(struct message *msg)
     if (msg->size > cursor) {
 	/* fill in the packet */
 	pkt.data[0] = msg->size-cursor;
-    *((seq_nr *)(pkt.data + 1)) = seq;
-	memcpy(pkt.data+HEADER_SIZE, msg->data+cursor, pkt.data[0]);
+	memcpy(pkt.data+header_size, msg->data+cursor, pkt.data[0]);
 
-    unsigned short cs = gen_checksum(pkt.data+HEADER_SIZE, msg->size-cursor);
-    *((unsigned short *)(pkt.data + PAYLOAD_HEADER_SIZE + SEQ_SIZE)) = cs;
-
-    if (nbuffered < WINDOW_SIZE) {
-        send(&pkt);
-    } else {
-        suspend(&pkt);
-    }
-    inc_seq();
+	/* send it out through the lower layer */
+	Sender_ToLowerLayer(&pkt);
     }
 }
 
@@ -328,34 +82,9 @@ void Sender_FromUpperLayer(struct message *msg)
    sender */
 void Sender_FromLowerLayer(struct packet *pkt)
 {
-    int payload_size = pkt->data[0];
-    unsigned short checksum = *((unsigned short *)(pkt->data + PAYLOAD_HEADER_SIZE + SEQ_SIZE));
-    if (!verify_checksum(pkt->data+HEADER_SIZE, payload_size, checksum))
-        return;
-
-    seq_nr actual_ack = *((seq_nr *)(pkt->data + 1));
-    while(between(ack, actual_ack, seq)) {
-        nbuffered = nbuffered - 1;
-        while(nbuffered < WINDOW_SIZE && !buffer_empty()){
-            buffer *p = buffer_pop();
-            send(&(p->pkt));
-            free(p);
-        }
-        stop_timer(ack);
-        inc_ack();
-    }
 }
 
 /* event handler, called when the timer expires */
 void Sender_Timeout()
 {
-    seq_nr a_seq = ack;
-    if (timer_list) destroy(timer_list);
-    timer_list = timer_tail = NULL;
-    for (seq_nr i = 0; i < nbuffered; i++) {
-        Sender_ToLowerLayer(window + (a_seq % WINDOW_SIZE));
-        register_timer(DEFAULT_TIMEOUT, a_seq);
-        a_seq++;
-    }
 }
-
