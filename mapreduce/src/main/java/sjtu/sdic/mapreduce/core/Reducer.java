812fa0b73de41ea5a27deea1e4401ed40fb7f473
artor1os <875241499@qq.com>
2020-06-07 02:54:50 +0800

    mapreduce

---
+++ mapreduce/src/main/java/sjtu/sdic/mapreduce/core/Reducer.java
@@ -0,0 +1,82 @@
+package sjtu.sdic.mapreduce.core;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import sjtu.sdic.mapreduce.common.KeyValue;
+import sjtu.sdic.mapreduce.common.Utils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Created by Cachhe on 2019/4/19.
+ */
+public class Reducer {
+
+    /**
+     * 
+     * 	doReduce manages one reduce task: it should read the intermediate
+     * 	files for the task, sort the intermediate key/value pairs by key,
+     * 	call the user-defined reduce function {@code reduceF} for each key,
+     * 	and write reduceF's output to disk.
+     * 	
+     * 	You'll need to read one intermediate file from each map task;
+     * 	{@code reduceName(jobName, m, reduceTask)} yields the file
+     * 	name from map task m.
+     *
+     * 	Your {@code doMap()} encoded the key/value pairs in the intermediate
+     * 	files, so you will need to decode them. If you used JSON, you can refer
+     * 	to related docs to know how to decode.
+     * 	
+     *  In the original paper, sorting is optional but helpful. Here you are
+     *  also required to do sorting. Lib is allowed.
+     * 	
+     * 	{@code reduceF()} is the application's reduce function. You should
+     * 	call it once per distinct key, with a slice of all the values
+     * 	for that key. {@code reduceF()} returns the reduced value for that
+     * 	key.
+     * 	
+     * 	You should write the reduce output as JSON encoded KeyValue
+     * 	objects to the file named outFile. We require you to use JSON
+     * 	because that is what the merger than combines the output
+     * 	from all the reduce tasks expects. There is nothing special about
+     * 	JSON -- it is just the marshalling format we chose to use.
+     * 	
+     * 	Your code here (Part I).
+     * 	
+     * 	
+     * @param jobName the name of the whole MapReduce job
+     * @param reduceTask which reduce task this is
+     * @param outFile write the output here
+     * @param nMap the number of map tasks that were run ("M" in the paper)
+     * @param reduceF user-defined reduce function
+     */
+    public static void doReduce(String jobName, int reduceTask, String outFile, int nMap, ReduceFunc reduceF) {
+        Mapper.writeFile(outFile, JSON.toJSONString(IntStream.range(0, nMap)
+                .mapToObj(m -> JSON.parseArray(readAndDeleteFile(Utils.reduceName(jobName, m, reduceTask)), KeyValue.class))
+                .flatMap(Collection::stream)
+                .collect(Collectors.groupingBy(kv -> kv.key))
+                .entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        e -> reduceF.reduce(e.getKey(), e.getValue().stream()
+                                .map(kv -> kv.value).toArray(String[]::new))))));
+    }
+
+    private static String readAndDeleteFile(String file) {
+        String contents = Mapper.readFile(file);
+        try {
+            Files.delete(Paths.get(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return contents;
+    }
+}
